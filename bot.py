d533865066f232a9c60791e51054019b29b5b277
zombie <dk@zombie.dev>
2021-02-11 17:52:31 -0500

    added logging and comments

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
+import json
 import os
 
+import discord
 import requests
 from bs4 import BeautifulSoup
+from discord import message
 from discord.ext import commands
+from discord.guild import Guild
 from dotenv import load_dotenv
 
+# use environment file for token key
 load_dotenv(verbose=True)
 DISCORD_TOKEN = os.getenv("TOKEN")
 
@@ -12,11 +17,22 @@ bot = commands.Bot(command_prefix='!')
 
 @bot.event
 async def on_ready():
-    print('TendieTracker online!')
-
+    print("TendierTracker online")
+    
 @bot.command()
 async def stock(ctx, ticker):
-    await ctx.send(stock_quote(ticker))
+    id = ctx.guild.id
+    name = ctx.guild.name
+    print(str(id) + ":" + name)
+    await ctx.channel.send(stock_quote(ticker))
+
+@bot.command()
+async def crypto(ctx, pair):
+    id = ctx.guild.id
+    name = ctx.guild.name
+    print(str(id) + ":" + name)
+    await ctx.channel.send(crypto_quote(pair))
+
 
 #scrapes CNN market stock pricing
 def stock_quote(ticker):
@@ -33,35 +49,42 @@ def stock_quote(ticker):
 
 def crypto_quote(query):
     try:
+        # split query coin:currency pair into variables for URL
         store = query.split(':')
 
         coin = store[0]
 
         currency = store[1]
 
+        # use formatted command for URL query.
         url = 'https://query1.finance.yahoo.com/v7/finance/quote?&symbols=' \
             + coin \
             + '-' \
             + currency
 
+        # get request for url above through requests library.
         page = requests.get(url)
 
+        # create the soup from the get request above.
         soup = BeautifulSoup(page.text, 'html.parser')
 
+        # load JSON API response for scraping.
         coin_json = json.loads(soup.text)
 
+        # pulling text out of json for crypto regular price
         price = coin_json['quoteResponse']['result'][0]['regularMarketPrice']
         if price == None:
             return "No pricing information for that pair."
 
         return price
 
+    # catch errors related to API calls failing.
     except IndexError:
         print("Coin or currency pair price information doesn't exist. Use <cointicker>:<currency> ex. BTC:USD")
 
     except KeyError:
         print("Coin or currency pair price information doesn't exist. Use <cointicker>:<currency> ex. BTC:USD")
-        
+
     except Exception as e:
         print(repr(e))
 
